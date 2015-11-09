331d121bf229595a296e422ff977a10cbb64da69
KiraBurova <coca-cola-from-hell@yandex.ru>
2015-11-09 22:16:48 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+        $("#search-icon").click(function () {
+            $("#advanced_search").slideToggle("slow");
+        });
+        $("#show_listing").click(function () {
+            $(".hide").slideToggle("slow");
+        });
+    });
