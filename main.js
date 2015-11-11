afdfb85a03cfe84f31fb871b46b3e4245ffb2580
KiraBurova <coca-cola-from-hell@yandex.ru>
2015-11-11 23:59:36 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-        $("#search-icon").click(function () {
-            $("#advanced_search").slideToggle("slow");
-        });
-        $("#show_listing").click(function () {
-            $(".hide").slideToggle("slow");
-        });
-    });
