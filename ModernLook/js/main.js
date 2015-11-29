6562e221fd07018c2f019602b7cfc72ae94fe359
KiraBurova <coca-cola-from-hell@yandex.ru>
2015-11-29 17:55:54 +0300

    Update main.js

--- ModernLook/js/main.js
+++ ModernLook/js/main.js
@@ -5,19 +5,3 @@ $(document).ready( function() {
     });
 });
 
-var wScroll = this.pageYOffset;
-var figures = document.querySelectorAll('.about .person-1');
-
-  function showPic(i) {
-    setTimeout(function(){
-      figures[i].classList.add('showing');
-    }, (700 * (Math.exp(i * 0.14)) - 700));
-  }
-
-  if(wScroll > document.querySelector('.about').scrollHeight + document.body.scrollTop - (window.innerHeight / 1)) {
-
-    for(var i = 0; i < figures.length; i++) {
-      showPic(i);
-    }
-
-  }
