4f9a11d00099279fa9a6ed4f0f56242bdbc71cf6
KiraBurova <coca-cola-from-hell@yandex.ru>
2017-06-28 21:09:22 +0300

    Kafeinate

---
+++ Kafeinate/assets/js/functions.js
@@ -0,0 +1,96 @@
+var upBtn = document.querySelector('.subscribe__up');
+
+//Табы
+document.addEventListener('DOMContentLoaded', function(e) {
+  // list = элементы a в .tabs__nav
+  var list = document.querySelectorAll('.menu__nav a');
+  // Превращаем list в array
+  list = Array.prototype.slice.call(list, 0);
+
+  // для каждого элемента list
+  list.forEach(function(el) {
+    // добавить событие click
+    el.addEventListener('click', function(e) {
+      e.preventDefault();
+      //tab = href выбранной a
+      var tab = document.querySelector(el.getAttribute('href'));
+      // Убираем класс active с a и контента
+      document.querySelector('.menu__nav .active')
+        .classList.remove('active');
+      document.querySelector('.menu__wrap .active')
+        .classList.remove('active');
+
+      // задаем класс active a и контенту
+      el.classList.add('active');
+      tab.classList.add('active');
+    });
+  });
+});
+
+//Слайдер
+function slideshowHeader() {
+  // list = элементы a в .tabs__nav
+  var slider = document.querySelectorAll('.controlls a');
+  // Превращаем list в array
+  slider = Array.prototype.slice.call(slider, 0);
+
+  // для каждого элемента list
+  slider.forEach(function(el) {
+    // добавить событие click
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      //tab = href выбранной a
+      var slide = document.querySelector(el.getAttribute('href'));
+      // Убираем класс active с a и контента
+
+
+
+      document.querySelector('.controlls .active')
+        .classList.remove('active');
+      document.querySelector('.slider__content .active')
+        .classList.remove('active');
+
+      // задаем класс active a и контенту
+      el.classList.add('active');
+      slide.classList.add('active');
+    });
+  });
+}
+function slideshow(e) {
+  var slider = document.querySelectorAll('.feedback__controlls a');
+  // Превращаем list в array
+  slider = Array.prototype.slice.call(slider, 0);
+
+  // для каждого элемента list
+  slider.forEach(function(el) {
+    // добавить событие click
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      //tab = href выбранной a
+      var slide = document.querySelector(el.getAttribute('href'));
+      // Убираем класс active с a и контента
+      document.querySelector('.feedback__controlls .active')
+        .classList.remove('active');
+      document.querySelector('.feedback__content .active')
+        .classList.remove('active');
+
+      // задаем класс active a и контенту
+      el.classList.add('active');
+      slide.classList.add('active');
+    });
+  });
+}
+//Скролл к верху страницы
+var timeOut;
+function scrollToTop() {
+  if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+    window.scrollBy(0, -100);
+    timeOut=setTimeout('scrollToTop()',10);
+  }
+  else clearTimeout(timeOut);
+}
+
+//Слайдер
+document.addEventListener('DOMContentLoaded', slideshowHeader);
+document.addEventListener('DOMContentLoaded', slideshow);
+upBtn.addEventListener('click', scrollToTop)
