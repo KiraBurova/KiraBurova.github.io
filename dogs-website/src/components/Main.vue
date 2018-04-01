90a02e6b690f6c57dde08b2fd11b2576e9cc4530
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-04-01 22:29:24 +0300

    add about section, basics styles for typography

--- dogs-website/src/components/Main.vue
+++ dogs-website/src/components/Main.vue
@@ -1,14 +1,19 @@
 <template>
-    <app-header></app-header>
+   <div>
+      <app-header></app-header>
+      <app-about></app-about>
+    </div>
 </template>
 
 <script>
 import Header from './Header.vue'
+import About from './About.vue'
 
 export default {
   name: 'mane',
   components: {
-    'app-header': Header
+    'app-header': Header,
+    'app-about': About
   }
 }
 </script>
